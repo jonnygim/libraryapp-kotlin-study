175ce65138779cb427073b7a712d3af5e17b30a1
Jonathan <jonnygim.dev@gmail.com>
2022-10-07 23:15:59 +0900

    [Feat] add book type

--- src/main/kotlin/com/group/libraryapp/LibraryAppApplication.kt
+++ src/main/kotlin/com/group/libraryapp/LibraryAppApplication.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class LibraryAppApplication
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) { 
     runApplication<LibraryAppApplication>(*args)
 }
